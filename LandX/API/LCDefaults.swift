09322f79c60e90b76cc23133a0ce5972c4a7b2dd
Podmno <podmno@icloud.com>
2023-07-29 10:06:02 +0800

    LCDefaults 引入

---
+++ LandX/API/LCDefaults.swift
@@ -0,0 +1,30 @@
+//
+//  LCDefaults.swift
+//  LandX
+//
+//  Created by Ki MNO on 2023/7/29.
+//
+
+import Foundation
+
+class LCDefaults {
+    
+    func setFirstInitDisplayDataStatus() {
+        
+        
+    }
+    
+    func loadFirstInitDisplayDataStatus() {
+        
+    
+    }
+    
+    
+    
+    func restoreDefaults() {
+        
+        
+    }
+    
+    
+}
